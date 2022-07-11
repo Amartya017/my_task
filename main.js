60046ed9ff76a5c3ce6b906871e70b747e9fc604
Amartya Sumukh Varma <amartyasumukh@gmail.com>
2022-07-11 11:22:15 +0530

    Button added

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-alert('Welcome to task page');
-alert('You shall find all your alloted tasks here');
+$(document).ready(function(){
+    var tablewrap = document.getElementById('dt');
+    tablewrap.classList.toggle('hidden')
+});
+function myfunction() {
+  var tablewrap = document.getElementById('dt');
+  tablewrap.classList.toggle('hidden')
+  var btnvalue = document.getElementById('cm').value;
+  if(btnvalue == 'Expand')
+  {
+    document.getElementById('cm').value = 'Collapse';
+  }
+  else{
+    document.getElementById('cm').value = 'Expand';
+  }
+};
+function myfunction2() {
+    var tablewrap = document.getElementById('dt');
+    tablewrap.classList.toggle('hidden')
+};
